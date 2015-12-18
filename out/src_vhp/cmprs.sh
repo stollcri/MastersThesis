6649a29107d932a78121f4f4622d41a29ae7bb41
Christopher Stoll <stollcri@gmail.com>
2015-12-17 22:37:41 -0500

    Script to perform compression comparisons

---
+++ out/src_vhp/cmprs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cp vhf-ankle.vtk vhf-ankle-bzip2.vtk
+cp vhf-ankle.vtk vhf-ankle-compress.vtk
+cp vhf-ankle.vtk vhf-ankle-gzip.vtk
+cp vhf-ankle.vtk vhf-ankle-zip.vtk
+bzip2 vhf-ankle-bzip2.vtk
+compress vhf-ankle-compress.vtk
+gzip vhf-ankle-gzip.vtk
+zip vhf-ankle-zip.vtk.zip vhf-ankle-zip.vtk
+
+cp vhf-head.vtk vhf-head-bzip2.vtk
+cp vhf-head.vtk vhf-head-compress.vtk
+cp vhf-head.vtk vhf-head-gzip.vtk
+cp vhf-head.vtk vhf-head-zip.vtk
+bzip2 vhf-head-bzip2.vtk
+compress vhf-head-compress.vtk
+gzip vhf-head-gzip.vtk
+zip vhf-head-zip.vtk.zip vhf-head-zip.vtk
+
+cp vhf-hip.vtk vhf-hip-bzip2.vtk
+cp vhf-hip.vtk vhf-hip-compress.vtk
+cp vhf-hip.vtk vhf-hip-gzip.vtk
+cp vhf-hip.vtk vhf-hip-zip.vtk
+bzip2 vhf-hip-bzip2.vtk
+compress vhf-hip-compress.vtk
+gzip vhf-hip-gzip.vtk
+zip vhf-hip-zip.vtk.zip vhf-hip-zip.vtk
+
+cp vhf-knee.vtk vhf-knee-bzip2.vtk
+cp vhf-knee.vtk vhf-knee-compress.vtk
+cp vhf-knee.vtk vhf-knee-gzip.vtk
+cp vhf-knee.vtk vhf-knee-zip.vtk
+bzip2 vhf-knee-bzip2.vtk
+compress vhf-knee-compress.vtk
+gzip vhf-knee-gzip.vtk
+zip vhf-knee-zip.vtk.zip vhf-knee-zip.vtk
+
+cp vhm-head.vtk vhm-head-bzip2.vtk
+cp vhm-head.vtk vhm-head-compress.vtk
+cp vhm-head.vtk vhm-head-gzip.vtk
+cp vhm-head.vtk vhm-head-zip.vtk
+bzip2 vhm-head-bzip2.vtk
+compress vhm-head-compress.vtk
+gzip vhm-head-gzip.vtk
+zip vhm-head-zip.vtk.zip vhm-head-zip.vtk
+
+cp vhm-hip.vtk vhm-hip-bzip2.vtk
+cp vhm-hip.vtk vhm-hip-compress.vtk
+cp vhm-hip.vtk vhm-hip-gzip.vtk
+cp vhm-hip.vtk vhm-hip-zip.vtk
+bzip2 vhm-hip-bzip2.vtk
+compress vhm-hip-compress.vtk
+gzip vhm-hip-gzip.vtk
+zip vhm-hip-zip.vtk.zip vhm-hip-zip.vtk
